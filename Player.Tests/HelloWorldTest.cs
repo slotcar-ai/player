6630c7c4f5d0e4715fca8dad1225e4e2567af7c9
Teodor Ande Elstad <teodor@andeelstad.com>
2018-08-06 18:51:53 +0200

    New empty test-project

---
+++ Player.Tests/HelloWorldTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Player.Tests
+{
+    public class HelloWorldTest
+    {
+        [Fact]
+        public void XunitShouldWork()
+        {
+            Assert.True(true);
+        }
+    }
+}
